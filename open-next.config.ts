04e782439a8f2f28d13048e8f28ee08023d9381d
Josue Sanchez <josue.sanchez@tokyotechies.com>
2025-11-07 10:38:47 +0900

    changes for build

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,14 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig({
+const config = defineCloudflareConfig({
   // Uncomment to enable R2 cache,
   // It should be imported as:
   // `import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";`
   // See https://opennext.js.org/cloudflare/caching for more details
   // incrementalCache: r2IncrementalCache,
 });
+
+// Avoid letting OpenNext call `pnpm build`, which would invoke this adapter recursively.
+config.buildCommand = "next build";
+
+export default config;
